1a0f92e8ae466afb721b41dc5f391b36e498425b
y_uuki <yuki.tsubo@gmail.com>
2018-03-08 00:44:47 +0900

    Prepare cloudformation execution

--- stacker/stacker.py
+++ stacker/stacker.py
@@ -2,24 +2,72 @@
 # coding: utf-8
 
 import optparse
+import os
+import subprocess
 import sys
 from . import version
 
+cfn_dryrun = False
 
-def action_create(stack, args):
+
+def print_command(*cmd):
+    print("\033[92m", ' '.join(cmd))
+
+
+def print_error(msg):
+    print("\033[91m", msg, file=sys.stderr)
+
+
+def cfn(action, arg):
+    print_command(f"aws cloudformation {action} {arg}")
+    if not cfn_dryrun:
+        subprocess.call("aws", "cloudformation", cmd.split())
+
+
+def _template_body(file):
+    if os.path.exists(file):
+        return f"--template-body file://{file}"
+    else:
+        print_error(f"template file {file} not found")
+        sys.exit(1)
+
+
+def _parameters(file):
+    if os.path.exists(file):
+        return f"--parameters file://{file}"
+    else:
+        return ''
+
+
+def _stack_policy_body(file):
+    if os.path.exists(file):
+        return f"--stack-policy-body file://{file}"
+    else:
+        print_error(f"policy file {file} not found")
+        sys.exit(1)
+
+
+def _capabilities(capabilities):
+    if capabilities == '':
+        return ''
+    else:
+        return f"--capabilities {capabilities}"
+
+
+def action_create(stack, files, opts):
     pass
 
 
-def action_update(stack, args):
+def action_update(stack, files, opts):
     pass
 
 
-def action_delete(stack, args):
+def action_delete(stack, files, opts):
     pass
 
 
-def action_validate(stack, args):
-    pass
+def action_validate(stack, files, opts):
+    cfn('validate-template', _template_body(files['template']))
 
 
 help_text = """%prog [OPTIONS] ACTION STACK
@@ -39,31 +87,54 @@ Options:
 """
 
 
+class UsageError(BaseException):
+    pass
+
+
 def main():
     parser = optparse.OptionParser(
         usage=help_text,
         version='%prog v'+version,
     )
-    parser.add_option('-s', '--stack-name', dest='stackname')
+    parser.add_option('-s', '--stack-name', dest='stack_name')
     parser.add_option('-c', '--capabilities', dest='capabilities')
     parser.add_option('-e', '--environment', dest='environment')
     parser.add_option('-p', '--profile', dest='profile')
-    parser.add_option('--dry-run', action='store_true')
+    parser.add_option('--dry-run', dest='dryrun', action='store_true')
 
     opts, args = parser.parse_args()
     if len(args) != 2:
         parser.print_help()
         sys.exit(129)
+
+    global cfn_dryrun
+    cfn_dryrun = opts.dryrun
+
+    if opts.profile is not None:
+        os.environ['AWS_PROFILE'] = opts.profile
+
+    cur_dir = os.getcwd()
+    stacks_dir = f"{cur_dir}/stacks"
+
+    action, stack = args[0], args[1]
+    project = os.path.basename(os.path.abspath(stacks_dir+'../'))
+    environment = opts.environment if opts.environment is not None else 'prod'
+    stack_name = opts.stack_name if opts.stack_name is not None else f"{project}-{environment}-{stack}"
+
+    files = {
+        "template": f"{stacks_dir}/{stack}/template.yml",
+        "parameter": f"{stacks_dir}/{stack}/parameters/{environment}.yml",
+        "policy": f"{stacks_dir}/{stack}/policy.yml",
+    }
     try:
-        action, stack = args[0], args[1]
         if action == 'create':
-            action_create(stack, opts)
+            action_create(stack_name, files, opts)
         elif action == 'update':
-            action_update(stack, opts)
+            action_update(stack_name, files, opts)
         elif action == 'delete':
-            action_delete(stack, opts)
+            action_delete(stack_name, files, opts)
         elif action == 'validate':
-            action_validate(stack, args)
+            action_validate(stack_name, files, opts)
         else:
             raise UsageError()
     except UsageError:
